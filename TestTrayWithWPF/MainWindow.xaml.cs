ab8a2cbd74657c4f61781190f6cd71aca2a060a8
George Legun <george.legun@kbclab.ro>
2025-10-21 15:37:56 +0300

    add button to test ElementHost.EnableModelessKeyboardIterop

--- TestTrayWithWPF/MainWindow.xaml.cs
+++ TestTrayWithWPF/MainWindow.xaml.cs
@@ -9,16 +9,17 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace TestTrayWithWPF
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-    }
+namespace TestTrayWithWPF {
+   /// <summary>
+   /// Interaction logic for MainWindow.xaml
+   /// </summary>
+   public partial class MainWindow : Window {
+      public MainWindow() {
+         InitializeComponent();
+      }
+
+      private void TestBtn_Click(object sender, RoutedEventArgs e) {
+         new MainWindow().Show();
+      }
+   }
 }
